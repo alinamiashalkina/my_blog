55396dc40e1aae595fd69dae77ebc6ba9ba15d7c
Alina Miashalkina <alin.ka@list.ru>
2024-11-17 20:55:41 +0300

    Customized admin panel for user and post

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 2
+    readonly_fields = ("create_date",)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_date",)
+    fieldsets = (
+        ("Post Description", {
+            "fields": ("title", "author", "publication_date",)
+        }),
+        ("More Details", {
+            "fields": ("post_text",)
+        })
+    )
+    readonly_fields = ("publication_date",)
+    search_fields = ("title", "post_text",)
+    ordering = ("title",)
+    list_filter = ("publication_date", "author",)
+    inlines = [CommentInline]
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    search_fields = ("first_name", "last_name")
